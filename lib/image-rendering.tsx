8fa1548f6e270fd30be8b6551888ac762d2a8c32
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-05 11:22:04 +0200

    Fix: Remove fallback url as a param, use constant

--- lib/image-rendering.tsx
+++ lib/image-rendering.tsx
@@ -1,10 +1,9 @@
 import { ImageBackgroundProps } from "react-native";
 const fallbackImage = require('../assets/images/image_placeholder.jpeg');
 
-export function getImageSource (url: string, fallbackUrl: string): ImageBackgroundProps["source"] {
+export function getImageSource (url: string): ImageBackgroundProps["source"] {
   if (url) {
     return { uri: url }
   }
   return fallbackImage;
-  // return require(fallbackUrl);
 }
