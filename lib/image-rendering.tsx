3232d60f16e9b681c22ad851805b8e79c84f2de3
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-05 10:58:20 +0200

    Fix: Use static fallback image for log

--- lib/image-rendering.tsx
+++ lib/image-rendering.tsx
@@ -1,8 +1,10 @@
 import { ImageBackgroundProps } from "react-native";
+const fallbackImage = require('../assets/images/image_placeholder.jpeg');
 
 export function getImageSource (url: string, fallbackUrl: string): ImageBackgroundProps["source"] {
   if (url) {
     return { uri: url }
   }
-  return require(fallbackUrl);
+  return fallbackImage;
+  // return require(fallbackUrl);
 }
