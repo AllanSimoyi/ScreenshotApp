f998f371afc01df55db3893a1875b195b27317b9
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-06 15:35:50 +0200

    Fix: Add use of useProfileDetails for debugging purposes

--- screens/InboxScreen.tsx
+++ screens/InboxScreen.tsx
@@ -14,7 +14,7 @@ export default function InboxScreen (_: RootTabScreenProps<'Inbox'>) {
   // const [signInModalIsOpen, setSignInModalIsOpen] = useState(false);
   // const [signUpModalIsOpen, setSignUpModalIsOpen] = useState(false);
   // const [message, setMessage] = useState("");
-  // const { isLoading, details, setDetails, error, setError, setIsRetryToggle, } = useProfileDetails();
+  const { isLoading, details, setDetails, error, setError, setIsRetryToggle, } = useProfileDetails();
   const query = useMessages('messages');
   // const mutation = useRecordMessage({
   //   onError: (error) => setError((error as any).toString()),
@@ -47,73 +47,60 @@ export default function InboxScreen (_: RootTabScreenProps<'Inbox'>) {
           {query?.error?.message}
         </CustomError>
       )}
+      {/* {error && (
+        <CustomError retry={() => setIsRetryToggle(prevState => !prevState)} style={{ height: "100%" }}>
+          {error}
+        </CustomError>
+      )} */}
+      {(query.isLoading || isLoading) && <CustomSkeletons num={4} />}
+      {/* {!isLoading && (
+        <SignIn
+          isOpen={signInModalIsOpen}
+          setIsOpen={setSignInModalIsOpen}
+          updateProfileDetails={setDetails}
+          openSignUpModal={() => setSignUpModalIsOpen(true)}
+        />
+      )} */}
+      {/* {!isLoading && (
+        <SignUp
+          isOpen={signUpModalIsOpen}
+          updateProfileDetails={setDetails}
+          setIsOpen={setSignUpModalIsOpen}
+          openSignInModal={() => setSignInModalIsOpen(true)}
+        />
+      )} */}
+      {/* {Boolean(messages) && (
+        <FlatList
+          data={messages}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item: report }) => (
+            <VStack alignItems="stretch" p={2}>
+              <VStack alignItems="stretch" p={2} style={{ backgroundColor: "rgba(255, 255, 255, 0.24)", borderRadius: 10 }}>
+                <Text color="yellow.600" fontSize={"lg"}>
+                  {report.sender.username}
+                </Text>
+                <Text color="white" fontSize={"md"}>
+                  {report.content}
+                </Text>
+              </VStack>
+              <HStack justifyContent="flex-end" alignItems="center" p={2}>
+                <Text color="white" fontSize={"xs"}>
+                  {dayjs(report.createdAt).format('hh:mm a')}
+                </Text>
+              </HStack>
+            </VStack>
+          )}
+        />
+      )} */}
+      {/* {Boolean(details.userId) && (
+        <VStack justifyContent="center" alignItems="stretch" p={2} style={{ position: "absolute", bottom: 0, left: 0, width: "100%", backgroundColor: "#000" }}>
+          <SendMessage
+            message={message}
+            setMessage={setMessage}
+            sendMessage={sendMessage}
+          />
+        </VStack>
+      )} */}
     </VStack>
-    // <VStack alignItems="stretch" px={0} pb={16} style={{ height: "100%" }}>
-    //   <HStack alignItems="center" py={2}>
-    //     <Text bold fontSize="md" px="3" color="#fff">
-    //       Messages
-    //     </Text>
-    //     <Flex flexGrow={1} />
-    //   </HStack>
-    //   {query.isError && (
-    //     <CustomError retry={() => query.refetch()} style={{ height: "100%" }}>
-    //       {query?.error?.message}
-    //     </CustomError>
-    //   )}
-    //   {error && (
-    //     <CustomError retry={() => setIsRetryToggle(prevState => !prevState)} style={{ height: "100%" }}>
-    //       {error}
-    //     </CustomError>
-    //   )}
-    //   {(query.isLoading || isLoading) && <CustomSkeletons num={4} />}
-    //   {!isLoading && (
-    //     <SignIn
-    //       isOpen={signInModalIsOpen}
-    //       setIsOpen={setSignInModalIsOpen}
-    //       updateProfileDetails={setDetails}
-    //       openSignUpModal={() => setSignUpModalIsOpen(true)}
-    //     />
-    //   )}
-    //   {!isLoading && (
-    //     <SignUp
-    //       isOpen={signUpModalIsOpen}
-    //       updateProfileDetails={setDetails}
-    //       setIsOpen={setSignUpModalIsOpen}
-    //       openSignInModal={() => setSignInModalIsOpen(true)}
-    //     />
-    //   )}
-    //   {Boolean(messages) && (
-    //     <FlatList
-    //       data={messages}
-    //       keyExtractor={(_, index) => index.toString()}
-    //       renderItem={({ item: report }) => (
-    //         <VStack alignItems="stretch" p={2}>
-    //           <VStack alignItems="stretch" p={2} style={{ backgroundColor: "rgba(255, 255, 255, 0.24)", borderRadius: 10 }}>
-    //             <Text color="yellow.600" fontSize={"lg"}>
-    //               {report.sender.username}
-    //             </Text>
-    //             <Text color="white" fontSize={"md"}>
-    //               {report.content}
-    //             </Text>
-    //           </VStack>
-    //           <HStack justifyContent="flex-end" alignItems="center" p={2}>
-    //             <Text color="white" fontSize={"xs"}>
-    //               {dayjs(report.createdAt).format('hh:mm a')}
-    //             </Text>
-    //           </HStack>
-    //         </VStack>
-    //       )}
-    //     />
-    //   )}
-    //   {Boolean(details.userId) && (
-    //     <VStack justifyContent="center" alignItems="stretch" p={2} style={{ position: "absolute", bottom: 0, left: 0, width: "100%", backgroundColor: "#000" }}>
-    //       <SendMessage
-    //         message={message}
-    //         setMessage={setMessage}
-    //         sendMessage={sendMessage}
-    //       />
-    //     </VStack>
-    //   )}
-    // </VStack>
   );
 }
