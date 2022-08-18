e4bad350c887d1f6044b485f4c983c58bcf70e29
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-18 11:47:46 +0200

    Refactor: Replace flexes with stacks

--- components/send-message.tsx
+++ components/send-message.tsx
@@ -1,5 +1,5 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { Flex, IconButton, Input } from "native-base";
+import { Flex, HStack, IconButton, Input, VStack } from "native-base";
 
 interface Props {
   message: string;
@@ -10,56 +10,18 @@ interface Props {
 export function SendMessage (props: Props) {
   const { message, setMessage, sendMessage } = props;
   return (
-    <Flex
-      flexDirection="row"
-      justify="flex-start"
-      align="stretch"
-      style={{
-        borderRadius: 10,
-        backgroundColor: "rgba(255, 255, 255, 0.24)"
-      }}
-    >
-      <Flex
-        flexDirection="column"
-        justify="center"
-        align="center"
-        p="2"
-        style={{ flexGrow: 1 }}
-      >
+    <HStack alignItems="stretch" style={{ borderRadius: 10, backgroundColor: "rgba(255, 255, 255, 0.24)" }}>
+      <VStack justifyContent="center" alignItems="center" p={2} style={{ flexGrow: 1 }}>
         <Input
-          value={message}
-          onChangeText={(text) => setMessage(text)}
-          size="xl"
-          fontWeight="bold"
-          color="yellow.600"
-          width="100%"
-          placeholder="Type Something..."
-          variant="rounded"
-          borderColor="yellow.600"
-          borderWidth="1"
-          my="2"
-          py="2"
-          px="4"
+          value={message} onChangeText={(text) => setMessage(text)}
+          fontWeight="bold" variant="rounded" placeholder="Type Something..."
+          color="yellow.600" borderColor="yellow.600" borderWidth="1"
+          size="xl" width="100%" my={2} py={2} px={4}
         />
-      </Flex>
-      <Flex
-        flexDirection="column"
-        justify="center"
-        align="center"
-        p="2"
-      >
-        <IconButton
-          colorScheme="yellow"
-          size="md"
-          // borderWidth={2}
-          // borderRadius="full"
-          // variant="outline"
-          onPress={sendMessage}
-          _icon={{
-            as: MaterialIcons,
-            name: "send"
-          }} />
-      </Flex>
-    </Flex>
+      </VStack>
+      <VStack justifyContent="center" alignItems="center" p={2}>
+        <IconButton colorScheme="yellow" size="md" onPress={sendMessage} _icon={{ as: MaterialIcons, name: "send" }} />
+      </VStack>
+    </HStack>
   )
 }
