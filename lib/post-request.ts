eb38f57feb07a60134d6d18e7505a55fb3122462
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-18 14:14:41 +0200

    Refactor: Remove spaces and dontLog

--- lib/post-request.ts
+++ lib/post-request.ts
@@ -7,14 +7,11 @@ export async function postRequest<ReturnType>(
 ): Promise<[ReturnType | undefined, Error | undefined]> {
   try {
     const headers = { "Content-Type": "application/json" }
-
     const response = await axios.post<ReturnType>(url, body, {
       headers,
       withCredentials: true,
-    })
-
-    console.log("POST REPONSE: ", url, response.data)
-
+    });
+    // console.log("POST REPONSE: ", url, response.data);
     return [response.data, undefined]
   } catch (reason: any) {
     return [undefined, new Error(reason?.message || FALLBACK_ERROR_MESSAGE)]
