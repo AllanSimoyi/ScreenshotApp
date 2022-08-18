9666fcfddad2d10478f047074d6ac96f0aa38755
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-18 15:31:10 +0200

    Fix: Adjust padding, flex grow

--- components/NoListItems.tsx
+++ components/NoListItems.tsx
@@ -7,7 +7,7 @@ interface Props {
 export function NoListItems (props: Props) {
   const { children } = props;
   return (
-    <VStack justifyContent={"center"} alignItems="center" p={4}>
+    <VStack justifyContent={"center"} alignItems="center" py={8} flexGrow={1}>
       <Text color="white" fontSize={"lg"}>
         {children}
       </Text>
