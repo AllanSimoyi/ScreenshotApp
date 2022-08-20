8981e6cc248a5195a1b078d12d03f03fef3609d7
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-20 12:17:37 +0200

    Fix: Replace literal with error message

--- lib/image-uploading.ts
+++ lib/image-uploading.ts
@@ -3,40 +3,17 @@ const UPLOAD_RESET = 'pbpinhve';
 
 export type UploadState = 'uploading' | 'uploaded' | 'error' | 'idle';
 
-// const CLOUDINARY_API_KEY = "993114726362865";
-// const CLOUDINARY_API_SECRET = "Rp2KAu3wG3ydmJrQQmxp-iZM-LY";
-
-// export async function handleImageUpload(file: File) {
 export async function handleImageUpload (blob: Blob) {
   return new Promise<string>((resolve, reject) => {
-
     const formData = new FormData();
-    // formData.append('file', file);
     formData.append('file', blob);
     formData.append('upload_preset', UPLOAD_RESET);
     formData.append('tags', 'rte');
     formData.append('context', '');
-
     const url = `https://api.cloudinary.com/v1_1/${ CLOUD_NAME }/upload`;
-
-    fetch(url, {
-      method: 'POST',
-      body: formData,
-    })
+    fetch(url, { method: 'POST', body: formData })
       .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        return resolve(result.url as string);
-      })
-      .catch((error) => {
-        console.log(error as any);
-        reject(new Error('Upload failed'));
-      });
-      // .catch(({ message }) => {
-      //   console.log('Something went wrong');
-      //   console.log(message as string);
-      //   reject(new Error('Upload failed'));
-      // });
+      .then((result) => resolve(result.url as string))
+      .catch((error) => reject(new Error('Upload failed')));
   });
 }
