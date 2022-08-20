be7e6a5f80707f8ab32ef6de3650533e56ae0dbd
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-20 12:01:38 +0200

    Refactor: Replace flexes with stacks

--- components/UploadThree.tsx
+++ components/UploadThree.tsx
@@ -1,37 +1,19 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Flex, Icon, Text } from 'native-base';
+import { Flex, Icon, Text, VStack } from 'native-base';
 
 export default function UploadThree () {
-
   return (
-    <Flex
-      direction="column"
-      justify="flex-start"
-      align="stretch"
-      px="0"
-      style={{ height: "100%" }}
-    >
+    <VStack alignItems="stretch" style={{ height: "100%" }}>
       <Flex flexGrow={1} />
-      <Flex
-        direction="column"
-        justify="center"
-        align="center"
-        px="0"
-        py="8"
-      >
+      <VStack justifyContent={"center"} alignItems="center" py={8}>
         <Text textAlign={"center"} fontWeight="extrabold" fontSize="4xl" color="#fff">
           Your Message Has Been Sent
         </Text>
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-          p="4">
+        <VStack justifyContent="center" alignItems="center" p={4}>
           <Icon as={Ionicons} color="yellow.600" name="checkmark-circle" size="6xl" />
-        </Flex>
-      </Flex>
+        </VStack>
+      </VStack>
       <Flex flexGrow={1} />
-    </Flex>
-
+    </VStack>
   );
 }
