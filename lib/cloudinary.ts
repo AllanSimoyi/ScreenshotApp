1d72c086505508071f240fb83dd0921329afcecb
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-23 18:22:18 +0200

    Fix: Use cloud name, use try catch

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -2,9 +2,13 @@ import { CloudConfig, CloudinaryImage, URLConfig } from "@cloudinary/url-gen";
 import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 
 export function createPostThumbnail (imageUrl: string) {
-  let cloudConfig = new CloudConfig({ cloudName: 'demo' });
+  let cloudConfig = new CloudConfig({ cloudName: 'dv2mivpiz' });
   let urlConfig = new URLConfig({ secure: true });
-  let myImage = new CloudinaryImage(imageUrl, cloudConfig, urlConfig);
-  myImage.resize(thumbnail().width("100%").height(250));
-  return myImage;
+  try {
+    let myImage = new CloudinaryImage(imageUrl, cloudConfig, urlConfig);
+    myImage.resize(thumbnail().width("100%").height(250));
+    return myImage;
+  } catch ({message}) {
+    return new CloudinaryImage("i7jcvgfwi8iqlbosxrzg", cloudConfig, urlConfig);
+  }
 }
