94e72a3b6816eb6a393a3d39b8cf965d07129c40
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-23 17:24:16 +0200

    Fix: Rename, rep as const the fallback image uri

--- lib/image-rendering.tsx
+++ lib/image-rendering.tsx
@@ -1,9 +1,9 @@
 import { ImageBackgroundProps } from "react-native";
-const fallbackImage = require('../assets/images/image_placeholder.jpeg');
+export const FALLBACK_IMAGE_URI = require('../assets/images/image_placeholder.jpeg');
 
 export function getImageSource (url: string): ImageBackgroundProps["source"] {
   if (url) {
     return { uri: url }
   }
-  return fallbackImage;
+  return FALLBACK_IMAGE_URI;
 }
