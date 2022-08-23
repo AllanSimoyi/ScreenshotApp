31caac19ae7bf40ae8ef070782128dab6c60e105
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-23 18:27:01 +0200

    Fix: Put all under try, remove url config

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -2,13 +2,13 @@ import { CloudConfig, CloudinaryImage, URLConfig } from "@cloudinary/url-gen";
 import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 
 export function createPostThumbnail (imageUrl: string) {
-  let cloudConfig = new CloudConfig({ cloudName: 'dv2mivpiz' });
-  let urlConfig = new URLConfig({ secure: true });
+  // let urlConfig = new URLConfig({ secure: true });
   try {
-    let myImage = new CloudinaryImage(imageUrl, cloudConfig, urlConfig);
+    // let myImage = new CloudinaryImage(imageUrl, cloudConfig, urlConfig);
+    let myImage = new CloudinaryImage(imageUrl, new CloudConfig({ cloudName: 'dv2mivpiz' }));
     myImage.resize(thumbnail().width("100%").height(250));
     return myImage;
   } catch ({message}) {
-    return new CloudinaryImage("i7jcvgfwi8iqlbosxrzg", cloudConfig, urlConfig);
+    return new CloudinaryImage("i7jcvgfwi8iqlbosxrzg", new CloudConfig({ cloudName: 'dv2mivpiz' }));
   }
 }
