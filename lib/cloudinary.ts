fff19fc2d6c1823803788db9209946fd0c704476
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-23 22:06:56 +0200

    Fix: Remove try catch, return url

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,14 +1,8 @@
-import { CloudConfig, CloudinaryImage, URLConfig } from "@cloudinary/url-gen";
+import { CloudConfig, CloudinaryImage } from "@cloudinary/url-gen";
 import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 
-export function createPostThumbnail (imageUrl: string) {
-  // let urlConfig = new URLConfig({ secure: true });
-  try {
-    // let myImage = new CloudinaryImage(imageUrl, cloudConfig, urlConfig);
-    let myImage = new CloudinaryImage(imageUrl, new CloudConfig({ cloudName: 'dv2mivpiz' }));
-    myImage.resize(thumbnail().width("100%").height(250));
-    return myImage;
-  } catch ({message}) {
-    return new CloudinaryImage("i7jcvgfwi8iqlbosxrzg", new CloudConfig({ cloudName: 'dv2mivpiz' }));
-  }
+export function getPostThumbnailUrl (imageUrl: string) {
+  let myImage = new CloudinaryImage(imageUrl, new CloudConfig({ cloudName: 'dv2mivpiz' }));
+  myImage.resize(thumbnail().width("100%").height(250));
+  return myImage.toURL();
 }
