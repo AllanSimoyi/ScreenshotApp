c931969865bfe5b5673ba94e15bc47d05ae53841
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-24 15:06:26 +0200

    Fix: Replace fallback image

--- lib/image-rendering.tsx
+++ lib/image-rendering.tsx
@@ -1,5 +1,5 @@
 import { ImageBackgroundProps } from "react-native";
-export const FALLBACK_IMAGE_URI = require('../assets/images/no_image_available.jpeg');
+export const FALLBACK_IMAGE_URI = require('../assets/images/no_image_available.png');
 
 export function getImageSource (url: string): ImageBackgroundProps["source"] {
   if (url) {
