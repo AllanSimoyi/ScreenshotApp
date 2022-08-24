3269901ac2892c52d2056cf063c0929c4d5e6e7b
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-24 10:15:32 +0200

    Fix: Add public id to arguments

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,8 +1,11 @@
 import { CloudConfig, CloudinaryImage } from "@cloudinary/url-gen";
 import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 
-export function getPostThumbnailUrl (imageUrl: string) {
-  let myImage = new CloudinaryImage(imageUrl, new CloudConfig({ cloudName: 'dv2mivpiz' }));
+export function getPostThumbnailUrl (publicId: string, imageUrl: string) {
+  if (!publicId) {
+    return imageUrl;
+  }
+  let myImage = new CloudinaryImage(publicId, new CloudConfig({ cloudName: 'dv2mivpiz' }));
   myImage.resize(thumbnail().width("100%").height(250));
   return myImage.toURL();
 }
