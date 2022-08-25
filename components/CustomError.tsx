50920c5c2cca1154e5fe3a6f230611527ce3d886
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-25 18:41:15 +0200

    Fix: Tweak custom error UI

--- components/CustomError.tsx
+++ components/CustomError.tsx
@@ -9,18 +9,18 @@ interface Props extends InterfaceVStackProps {
 export function CustomError (props: Props) {
   const { retry, children, ...restOfProps } = props;
   return (
-    <VStack justifyContent="center" alignItems="stretch" p={4} {...restOfProps}>
+    <VStack justifyContent="center" alignItems="stretch" {...restOfProps}>
       <Alert w="100%" status="error">
         <VStack space={2} flexShrink={1} w="100%">
-          <HStack space={2} flexShrink={1} alignItems="center">
+          <HStack space={2} flexShrink={1} justifyContent="center" alignItems="center">
             <Alert.Icon mt="1" />
             <Text fontSize="sm" color="coolGray.800" p="1">
               {children}
             </Text>
           </HStack>
-          <VStack justifyContent="center" alignItems="center">
-            <Button borderColor="black" onPress={retry} variant="outline">
-              RETRY
+          <VStack justifyContent="center" alignItems="stretch">
+            <Button borderColor="black" onPress={retry} variant="outline" py={2} px={4}>
+              <Text color="black">RETRY</Text>
             </Button>
           </VStack>
         </VStack>
