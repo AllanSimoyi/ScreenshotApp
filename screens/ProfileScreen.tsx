ac767adcdfba4eb3617beeb70dec44bbcb060994
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-25 10:08:02 +0200

    Fix: Revert back to sign in modal to push to prod

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,5 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Button, Flex, Icon, Image, ScrollView, Text, VStack } from 'native-base';
+import { Button, Flex, HStack, Image, ScrollView, Text, VStack } from 'native-base';
 import { useCallback, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Col, Grid, Row } from "react-native-easy-grid";
@@ -8,20 +7,22 @@ import { IMAGE_DEFAULT_SOURCE } from '../components/CustomImageBackground';
 import { EditProfile } from '../components/EditProfile';
 import { Loading } from '../components/Loading';
 import { SignIn } from '../components/SignIn';
+import { SignInComponent } from '../components/SignInComponent';
 import { SignUp } from '../components/SignUp';
 import { usePosts } from "../hooks/usePosts";
 import { useProfileDetails } from '../hooks/useProfileDetails';
 import { getPostThumbnailUrl } from '../lib/cloudinary';
 import { getImageSource } from "../lib/image-rendering";
 import { toMatrix } from '../lib/posts';
-import { ProfileDetails } from '../lib/users';
+import { ProfileDetails, User } from '../lib/users';
 import { RootTabScreenProps } from '../types';
 
 export default function ProfileScreen ({ }: RootTabScreenProps<'Profile'>) {
+  const [editModalisOpen, setEditModalIsOpen] = useState(false);
   const [signInModalIsOpen, setSignInModalIsOpen] = useState(false);
   const [signUpModalIsOpen, setSignUpModalIsOpen] = useState(false);
-  const [editModalisOpen, setEditModalIsOpen] = useState(false);
-  const { isLoading, details, setDetails, error, setError, setIsRetryToggle } = useProfileDetails();
+  // const [signInIsOpen, setSignInIsOpen] = useState(false);
+  const { isLoading, details, setDetails, error, setError, setIsRetryToggle, signOut } = useProfileDetails();
   const { refetch, ...query } = usePosts("profile");
   const refetchCallback = useCallback(() => refetch(), [refetch]);
   const handleEditedDetails = useCallback((editedDetails: Omit<ProfileDetails, "userId">) => {
@@ -34,9 +35,22 @@ export default function ProfileScreen ({ }: RootTabScreenProps<'Profile'>) {
     setError("");
     setIsRetryToggle(prevState => !prevState);
   }, [setIsRetryToggle]);
+  const openEditModal = useCallback(() => setEditModalIsOpen(true), []);
   const openSignInModal = useCallback(() => setSignInModalIsOpen(true), []);
   const openSignUpModal = useCallback(() => setSignUpModalIsOpen(true), []);
-  const openEditModal = useCallback(() => setEditModalIsOpen(true), []);
+  // const openSignIn = useCallback(() => setSignInIsOpen(true), []);
+  // const closeSignIn = useCallback(() => setSignInIsOpen(false), []);
+  // const onSignInSuccess = useCallback((newCurrentUser: User) => {
+  //   closeSignIn();
+  //   setDetails({
+  //     userId: newCurrentUser.id,
+  //     username: newCurrentUser.username,
+  //     phoneNumber: newCurrentUser.phoneNumber,
+  //   });
+  // }, [closeSignIn]);
+  // const handleBackFromSignIn = useCallback(() => {
+  //   closeSignIn();
+  // }, [closeSignIn]);
   const posts = query.data?.filter(post => post.userId === details.userId) || [];
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -48,6 +62,9 @@ export default function ProfileScreen ({ }: RootTabScreenProps<'Profile'>) {
           {error}
         </CustomError>
       )}
+      {/* {!Boolean(details.userId) && !isLoading && (
+        <SignInComponent onSuccess={onSignInSuccess} noBack={true} back={handleBackFromSignIn} />
+      )} */}
       {!Boolean(details.userId) && !isLoading && (
         <VStack justifyContent="center" alignItems="center" py={2} style={{ height: "100%" }}>
           <Button size="lg" px="6" colorScheme="yellow" variant="outline" borderColor="#fff"
@@ -101,17 +118,18 @@ export default function ProfileScreen ({ }: RootTabScreenProps<'Profile'>) {
               </Text>
             </VStack>
             <Flex flexGrow={1} />
-            <VStack justifyContent="center" alignItems="center" p={2}>
-              <Button
-                onPress={openEditModal}
-                leftIcon={<Icon as={Ionicons} color="#333" name="pencil" size="md" />}
-                size="lg" colorScheme="yellow" variant="outline" px="6"
-                borderColor="black" borderWidth={1} borderRadius={35}>
+            <HStack justifyContent="center" alignItems="center" p={2} space={4}>
+              <Button onPress={openEditModal} size="md" colorScheme="yellow" variant="outline" borderColor="black" borderWidth={1}>
                 <Text color="black" fontWeight={"bold"} fontSize="md">
                   Edit Profile
                 </Text>
               </Button>
-            </VStack>
+              <Button onPress={signOut} size="md" colorScheme="yellow" variant="outline" borderColor="black" borderWidth={1}>
+                <Text color="black" fontWeight={"bold"} fontSize="md">
+                  Log Out
+                </Text>
+              </Button>
+            </HStack>
           </VStack>
           <VStack alignItems="stretch" p={2}>
             <Text bold fontSize="md" px="3" pt="2" color="#fff">
