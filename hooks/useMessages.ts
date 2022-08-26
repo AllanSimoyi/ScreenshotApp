83fe98e14b869ac8e94022579ba734e2b02e6642
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-26 15:46:06 +0200

    Fix: Add params to record message mutation, add enabled prop to useMessages

--- hooks/useMessages.ts
+++ hooks/useMessages.ts
@@ -5,7 +5,7 @@ import { postRequest } from '../lib/post-request';
 import { URL_PREFIX } from '../lib/url-prefix';
 import { CreateMessage } from '../lib/validations';
 
-export function useMessages (queryKey: QueryKey) {
+export function useMessages (queryKey: QueryKey, enabled: boolean) {
   return useQuery<Message[], Error>(queryKey, async () => {
     const [result, err] = await getRequest<{ messages: Message[]; errorMessage: string }>(URL_PREFIX + "/api/messages");
     if (err) {
@@ -15,16 +15,17 @@ export function useMessages (queryKey: QueryKey) {
       throw new Error(result?.errorMessage);
     }
     return result?.messages || [];
-  });
+  }, { enabled });
 }
 
 interface useRecordMessageProps {
-  onError: (error: unknown) => void;
+  onMutate: (newMessage: CreateMessage) => Promise<any>;
+  onError: (error: unknown, newMessage: CreateMessage, context: any) => void;
   onSettled: () => void;
 }
 
 export function useRecordMessage (props: useRecordMessageProps) {
-  const { onError, onSettled } = props;
+  const { onMutate, onError, onSettled } = props;
   return useMutation(async (newMessage: CreateMessage) => {
     const [result, err] = await postRequest<{ message: Message; errorMessage: string; }>(URL_PREFIX + "/api/messages", newMessage);
     if (err) {
@@ -34,8 +35,5 @@ export function useRecordMessage (props: useRecordMessageProps) {
       throw new Error(result?.errorMessage);
     }
     return result?.message || undefined;
-  }, {
-    onError,
-    onSettled
-  });
+  }, { onMutate, onError, onSettled });
 }
