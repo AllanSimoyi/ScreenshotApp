9c5bc43d4e851ea243cefb78b8251874abd5f3ea
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-26 15:43:02 +0200

    Fix: Show EOF message conditionally

--- components/FlatListFooter.tsx
+++ components/FlatListFooter.tsx
@@ -4,14 +4,15 @@ interface Props {
   listName: string
   isLoadingMore: boolean
   atEndOfList: boolean
+  isEmptyList: boolean
 }
 
 export function FlatListFooter (props: Props) {
-  const {listName, isLoadingMore, atEndOfList} = props;
+  const {listName, isLoadingMore, atEndOfList, isEmptyList} = props;
   return (
     <VStack alignItems="center" p={4}>
       {isLoadingMore && <Spinner size="lg" color="yellow.600" />}
-      {atEndOfList && <Text fontSize="sm" color="white" p="1">End Of {listName}</Text>}
+      {atEndOfList && !isEmptyList && <Text fontSize="sm" color="white" p="1">End Of {listName}</Text>}
     </VStack>
   )
 }
