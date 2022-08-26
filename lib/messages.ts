fc8c5a82168cf795d1142babbdf054ceb882c5c7
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-26 15:44:13 +0200

    Feat: Create optimistic message fn

--- lib/messages.ts
+++ lib/messages.ts
@@ -1,4 +1,6 @@
+import { CurrentUser } from "../components/CurrentUserProvider";
 import { User } from "./users";
+import { CreateMessage } from "./validations";
 
 export interface Message {
   id: number;
@@ -13,4 +15,32 @@ export interface Message {
 
   createdAt: Date;
   updatedAt: Date;
+}
+
+export function createOptimisticMessage (newMessage: CreateMessage, currentUser: CurrentUser) {
+  const message: Message = {
+    id: 1,
+    senderId: newMessage.userId,
+    sender: {
+      id: currentUser.userId,
+      username: currentUser.username,
+      role: "Normal",
+      phoneNumber: currentUser.phoneNumber,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    receiverId: newMessage.receiverId,
+    receiver: {
+      id: newMessage.receiverId,
+      username: "ZIMCODD",
+      role: "Organization",
+      phoneNumber: "+263738083125",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    content: newMessage.content,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+  return message;
 }
