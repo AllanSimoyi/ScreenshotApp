b788d5731c86106946c0d26776158e33cb975d08
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-03 11:24:12 +0200

    Fix: Add padding

--- components/CustomError.tsx
+++ components/CustomError.tsx
@@ -9,7 +9,7 @@ interface Props extends InterfaceVStackProps {
 export function CustomError (props: Props) {
   const { retry, children, ...restOfProps } = props;
   return (
-    <VStack justifyContent="center" alignItems="stretch" {...restOfProps}>
+    <VStack py={2} justifyContent="center" alignItems="stretch" {...restOfProps}>
       <Alert w="100%" status="error">
         <VStack space={2} flexShrink={1} w="100%">
           <HStack space={2} flexShrink={1} justifyContent="center" alignItems="center">
