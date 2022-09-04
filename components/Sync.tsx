66458576d8ae44fa8c7d50ed201520bca6c971c2
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-04 15:31:45 +0200

    Feat: Create component to upload pending posts every 2 mins

---
+++ components/Sync.tsx
@@ -0,0 +1,77 @@
+import * as Network from 'expo-network';
+import { useCallback, useEffect, useState } from 'react';
+import { usePostMutation } from '../hooks/usePostMutation';
+import { db } from '../lib/db';
+import { Post } from '../lib/posts';
+
+interface Props {
+  children: React.ReactNode;
+  isLoadingComplete: boolean;
+}
+
+export function Sync (props: Props) {
+  const { children, isLoadingComplete } = props;
+  const [retryToggle, setRetryToggle] = useState(false);
+  const removePostFromLocalDB = useCallback((uuid: string) => {
+    db.transaction((tx) => {
+      tx.executeSql(`delete from posts where uuid = ?;`, [uuid])
+    });
+  }, []);
+  const { mutate: uploadPost } = usePostMutation({
+    onSuccess: async (newPost: Post | undefined) => {
+      if (newPost) {
+        removePostFromLocalDB(newPost.uuid);
+      }
+    },
+    onError: (_) => {},
+  });
+  const fetchLocalPosts = useCallback(() => {
+    return new Promise<[Post[], string | undefined]>((resolve, reject) => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          `select * from posts;`, [],
+          (_, { rows: { _array } }) => resolve([_array, undefined]),
+        )
+      }, (error) => resolve([[], error.message || "Something went wrong, please try again"])
+      );
+    })
+  }, []);
+  useEffect(() => {
+    if (isLoadingComplete) {
+      init();
+    }
+    const TWO_MINUTES_IN_MILLISECONDS = 60 * 2 * 1000;
+    const interval = setInterval(() => {
+      setRetryToggle(prevState => !prevState);
+    }, TWO_MINUTES_IN_MILLISECONDS);
+    async function init () {
+      try {
+        const [pendingPosts, err] = await fetchLocalPosts();
+        if (err) {
+          throw new Error(err);
+        }
+        const networkState = await Network.getNetworkStateAsync();
+        if (networkState.isInternetReachable) {
+          console.log("PENDING POSTS FOUND:", pendingPosts.length);
+          pendingPosts.forEach((post) => {
+            const { id, resourceUrl, createdAt, updatedAt, ...details } = post;
+            return uploadPost({
+              ...details,
+              userId: Number(details.userId),
+              resourceBase64: post.resourceUrl,
+              resourceType: "Image",
+              publicly: Boolean(details.publicly),
+            });
+          });
+        } else {
+          throw new Error("No internet connection");
+        }
+      } catch ({ message }) {
+        const errorMessage = JSON.stringify(message) as string || "Something went wrong, please try again";
+        console.error(errorMessage);
+      }
+    }
+    return () => clearInterval(interval);
+  }, [Network.getNetworkStateAsync, uploadPost, retryToggle]);
+  return (<>{children}</>)
+}
