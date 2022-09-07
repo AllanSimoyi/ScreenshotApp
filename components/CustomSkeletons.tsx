77180c3b3bfe45e264eb85c08f939eb143ad3b70
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-07 16:25:06 +0200

    Fix: Create fixed skeletons layout, factor out num array

--- components/CustomSkeletons.tsx
+++ components/CustomSkeletons.tsx
@@ -1,24 +1,28 @@
 import { ISkeletonProps, Skeleton, VStack } from "native-base";
 
 interface Props extends ISkeletonProps {
+  identifier: string;
   num: number;
 }
 
-export function CustomSkeletons (props: Props) {
-  const { num, ...restOfProps } = props;
-  return (
-    <VStack alignItems="stretch" p={4}>
-      {createNumberedArray(num).map((_, index) => (
-        <Skeleton startColor="coolGray.400" key={index.toString()} mb="4" h="40" rounded="10" {...restOfProps} />
-      ))}
-    </VStack>
-  )
-}
-
-function createNumberedArray (num: number) {
+const items = ((num: number) => {
   const result: number[] = [];
   for (let i = 0; i < num; i++) {
     result.push(num);
   }
   return result;
+})(4);
+
+export function CustomSkeletons (props: Props) {
+  const { num, identifier, ...restOfProps } = props;
+  return (
+    <VStack alignItems="stretch" p={4}>
+      {items.map(num => (
+        <Skeleton
+          startColor="coolGray.400" key={`${ identifier }-${ num.toString() }`}
+          mb="4" h="40" rounded="10" {...restOfProps}
+        />
+      ))}
+    </VStack>
+  )
 }
