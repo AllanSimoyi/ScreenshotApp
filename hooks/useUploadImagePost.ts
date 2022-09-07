d27024e85ff9a6019285bf6a0005d89d26743f86
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-07 16:29:48 +0200

    Feat: Create hooks to handle uploads, avoid race conditions and update local db

---
+++ hooks/useUploadImagePost.ts
@@ -0,0 +1,60 @@
+import { useCallback } from 'react';
+import { Post } from "../lib/posts";
+import { CreatePost } from '../lib/validations';
+import { useLocalDb } from './useLocalDB';
+import { usePostMutation } from './usePostMutation';
+
+interface Props {
+  currentUploads: string[];
+  addToCurrentUploads: (uuid: string) => void;
+  removeFromCurrentUploads: (uuid: string) => void;
+  onSuccess: (newPost: Post | undefined) => void;
+  onError: (error: unknown, post: Post) => void;
+}
+
+export function useUploadImagePost (props: Props) {
+  const { currentUploads, addToCurrentUploads, removeFromCurrentUploads } = props;
+  const { onSuccess, onError } = props;
+  const { writePostToLocalDB, removePostFromLocalDB } = useLocalDb();
+
+  const { mutate, ...mutation } = usePostMutation({
+    onSuccess: (newPost) => {
+      if (newPost) {
+        removeFromCurrentUploads(newPost.uuid);
+        removePostFromLocalDB(newPost.uuid);
+      }
+      onSuccess(newPost);
+    },
+    onError: (error, post) => {
+      removeFromCurrentUploads(post.uuid);
+      onError(error, post);
+    }
+  });
+
+  const handleImagePostMutation = useCallback(async (post: CreatePost, newPost: "newPost" | undefined) => {
+    if (newPost) {
+      const details: Post = {
+        ...post,
+        id: 1,
+        userId: Number(post.userId),
+        resourceUrl: post.resourceBase64 || "",
+        description: post.description || "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+      const err = await writePostToLocalDB(details);
+      if (err) {
+        return err;
+      }
+    }
+    const alreadyUploading = currentUploads.some(upload => upload === post.uuid);
+    if (alreadyUploading) {
+      return;
+    }
+    addToCurrentUploads(post.uuid);
+    mutate(post);
+    return undefined;
+  }, [addToCurrentUploads, currentUploads, mutate]);
+
+  return { ...mutation, handleImagePostMutation }
+}
---
+++ hooks/useUploadVideoPost.ts
@@ -0,0 +1,60 @@
+import { useCallback } from 'react';
+import { Post } from "../lib/posts";
+import { CreateVideoPost } from '../lib/validations';
+import { useLocalDb } from './useLocalDB';
+import { useVideoPostMutation } from './useVideoPostMutation';
+
+interface Props {
+  currentUploads: string[];
+  addToCurrentUploads: (uuid: string) => void;
+  removeFromCurrentUploads: (uuid: string) => void;
+  onSuccess: (newPost: Post | undefined) => void;
+  onError: (error: unknown, post: Post) => void;
+}
+
+export function useUploadVideoPost (props: Props) {
+  const { currentUploads, addToCurrentUploads, removeFromCurrentUploads } = props;
+  const { onSuccess, onError } = props;
+  const { writePostToLocalDB, removePostFromLocalDB } = useLocalDb();
+
+  const { mutate, ...mutation } = useVideoPostMutation({
+    onSuccess: (newPost) => {
+      if (newPost) {
+        removeFromCurrentUploads(newPost.uuid);
+        removePostFromLocalDB(newPost.uuid);
+      }
+      onSuccess(newPost);
+    },
+    onError: (error, post) => {
+      removeFromCurrentUploads(post.uuid);
+      onError(error, post);
+    },
+  });
+
+  const handleVideoPostMutation = useCallback(async (post: CreateVideoPost, newPost: "newPost" | undefined) => {
+    if (newPost) {
+      const details: Post = {
+        ...post,
+        id: 1,
+        userId: Number(post.userId),
+        resourceUrl: post.uri || "",
+        description: post.description || "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+      const err = await writePostToLocalDB(details);
+      if (err) {
+        return err;
+      }
+    }
+    const alreadyUploading = currentUploads.some(upload => upload === post.uuid);
+    if (alreadyUploading) {
+      return;
+    }
+    addToCurrentUploads(post.uuid);
+    mutate(post);
+    return undefined;
+  }, [addToCurrentUploads, currentUploads, mutate]);
+
+  return { ...mutation, handleVideoPostMutation }
+}
