9207868da98ca8584dc31dc994f8845742c24595
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-09-07 16:21:04 +0200

    Fix: Add fn to get a video thumbnail

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,4 +1,4 @@
-import { CloudConfig, CloudinaryImage } from "@cloudinary/url-gen";
+import { CloudConfig, CloudinaryImage, CloudinaryVideo } from "@cloudinary/url-gen";
 import { thumbnail } from "@cloudinary/url-gen/actions/resize";
 
 export function getPostThumbnailUrl (publicId: string | undefined, imageUrl: string) {
@@ -9,6 +9,18 @@ export function getPostThumbnailUrl (publicId: string | undefined, imageUrl: str
   myImage
     .resize(thumbnail().height(250))
     .format('auto')
+    .quality('auto');
+  return myImage.toURL();
+}
+
+export function getVideoThumbnail (publicId: string | undefined, imageUrl: string) {
+  if (!publicId) {
+    return imageUrl;
+  }
+  let myImage = new CloudinaryVideo(publicId, new CloudConfig({ cloudName: 'dv2mivpiz' }));
+  myImage
+    .format('jpg')
+    .resize(thumbnail().height(250))
     .quality('auto');
   return myImage.toURL();
 }
